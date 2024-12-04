0735e1ca38182dd4bd526dd838558205d9fb40b8
kbluuzy <manonk1@tcnj.edu>
2024-12-04 18:34:18 -0500

    perspective18

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+console.log("hello world!")
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
