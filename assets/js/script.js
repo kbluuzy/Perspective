872206b725ff012ad50d38365b52312d1ee3122a
kbluuzy <manonk1@tcnj.edu>
2024-12-12 17:14:15 -0500

    commit20

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,29 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+
+let mobileMenuOpen = false;
+
+$("#mobile-menu-button").click(function(){
+    
+    if(mobileMenuOpen == false) {
+        $("#main-menu").removeClass("mobile-display-none");
+        $("#mobile-menu-text").text("Close");
+        let close = $("#mobile-menu-icon").attr("src").replace("hamburger", "close");
+        console.log(close);
+        $("#mobile-menu-icon").attr("src", close);
+
+        mobileMenuOpen = true;
+    }
+
+    else {
+        $("#main-menu").addClass("mobile-display-none");
+        $("#mobile-menu-text").text("Menu");
+        let open = $("#mobile-menu-icon").attr("src").replace("close", "hamburger");
+
+        $("#mobile-menu-icon").attr("src", open);
+
+        mobileMenuOpen = false; 
+    }
+});
